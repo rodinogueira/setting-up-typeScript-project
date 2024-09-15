e874e2a641a27b43ea3d4b8320ebbf0e93d73d5b
Rodrigo Nogueira <rdg6design@gmail.com>
2024-09-15 17:29:00 -0300

    feat: setting .eslintrc.js config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser', // Use the TypeScript parser
+  parserOptions: {
+    ecmaVersion: 2018, // Use ECMAScript 2018 syntax
+    sourceType: 'module', // Allow import/export syntax
+  },
+  plugins: ['@typescript-eslint'], // Use the TypeScript plugin
+  rules: {
+    'quotes': ['error', 'single'], // Enforce single quotes
+    'semi': ['error', 'never'], // Disallow semicolons
+    '@typescript-eslint/no-empty-function': 'off', // Turn off the rule for empty functions
+    // outras regras personalizadas podem ser adicionadas aqui
+  },
+}
